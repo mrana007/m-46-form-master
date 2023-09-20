9aa89ec461e5c041501d494591c0622ef47b5b7d
mrana007 <rana.es.bc@gmail.com>
2023-09-21 03:39:07 +0600

    added hookForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import RefForm from './components/RefForm/RefForm'
+import HookForm from './components/HookForm/HookForm'
+// import RefForm from './components/RefForm/RefForm'
 // import StatefulForm from './components/StatefulForm/StatefulForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 
@@ -10,7 +11,8 @@ function App() {
       <h1>Form Master</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
-      <RefForm></RefForm>
+      {/* <RefForm></RefForm> */}
+      <HookForm></HookForm>
     </>
   )
 }
