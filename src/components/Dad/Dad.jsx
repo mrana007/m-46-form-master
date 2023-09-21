5b47bce4db06f85c56b3d7a6906dbe6e1bb8f6ee
mrana007 <rana.es.bc@gmail.com>
2023-09-22 01:59:11 +0600

    added useContext

---
+++ src/components/Dad/Dad.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import Brother from "../Brother/Brother";
+import Myself from "../Myself/Myself";
+import Sister from "../Sister/Sister";
+
+
+const Dad = ({asset}) => {
+    return (
+        <div>
+            <h2>Dad</h2>
+            <section className="flex">
+                <Myself asset={asset}></Myself>
+                <Brother></Brother>
+                <Sister></Sister>
+            </section>
+        </div>
+    );
+};
+
+export default Dad;
--- src/components/ReusableForm/ReusableForm.jsx
+++ src/components/ReusableForm/ReusableForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 
 
 const ReusableForm = ({formTitle, handleSubmit, submitBtnText = 'Submit', children}) => {
